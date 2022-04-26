09914db815efca76f2373f6f80b2a492c44cb9b9
Aleksey Kazarin <pt.akazarin@prosper.com>
2022-04-26 20:32:37 +0300

    Restore hardcoded value

--- src/main/java/hudson/plugins/view/dashboard/test/TestResult.java
+++ src/main/java/hudson/plugins/view/dashboard/test/TestResult.java
@@ -33,7 +33,7 @@ public class TestResult {
   }
 
   public double getSuccessPct() {
-    return tests != 0 ? ((double) success / tests) : 66d;
+    return tests != 0 ? ((double) success / tests) : 0d;
   }
 
   public int getFailed() {
