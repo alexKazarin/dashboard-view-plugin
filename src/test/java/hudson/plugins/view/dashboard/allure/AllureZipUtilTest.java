8f6746f87568aac90cfd92fa1e11b07b05b05df8
Aleksey Kazarin <alex.kazarin.mail@gmail.com>
2022-07-26 14:05:48 +0300

    Updated AllureZipUtilTest

--- src/test/java/hudson/plugins/view/dashboard/allure/AllureZipUtilTest.java
+++ src/test/java/hudson/plugins/view/dashboard/allure/AllureZipUtilTest.java
@@ -13,36 +13,51 @@ import org.junit.Test;
 
 public class AllureZipUtilTest {
 
+  private static final String ALLURE_REPORT_RESOURCE = "allure-report.zip";
+  private static final String ALLURE_REPORT_DIRECTORY = "allure-report";
+  private static final String ALLURE_REPORT_WIDGETS_LOCATION = "/widgets";
+
   /** Test of ListEntries, of class AllureZipUtils. */
   @Test
-  public void testListEntries() {
-    String pathToFile =
-        Objects.requireNonNull(getClass().getResource("allure-report.zip")).getPath();
-    String pathToElement = "allure-report".concat("/widgets");
-    ZipFile archive;
-    try {
-      archive = new ZipFile(pathToFile);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    List<ZipEntry> entries = listEntries(archive, pathToElement);
+  public void testListEntriesExistingPath() {
+    String pathToFile = Objects.requireNonNull(getClass().getResource(ALLURE_REPORT_RESOURCE)).getPath();
+    String pathToElement = ALLURE_REPORT_DIRECTORY.concat(ALLURE_REPORT_WIDGETS_LOCATION);
+
+    List<ZipEntry> entries = listEntries(initZipFile(pathToFile), pathToElement);
+
     assertNotNull(entries);
     assertEquals(15, entries.size());
   }
 
   @Test
+  public void testListEntriesNotExistingPath() {
+    String pathToFile = Objects.requireNonNull(getClass().getResource(ALLURE_REPORT_RESOURCE)).getPath();
+    String pathToElement = ALLURE_REPORT_DIRECTORY.concat("/random");
+
+    List<ZipEntry> entries = listEntries(initZipFile(pathToFile), pathToElement);
+
+    assertNotNull(entries);
+    assertEquals(0, entries.size());
+  }
+
+  @Test
   public void testListEntriesEmptyPath() {
-    String pathToFile =
-        Objects.requireNonNull(getClass().getResource("allure-report.zip")).getPath();
+    String pathToFile = Objects.requireNonNull(getClass().getResource(ALLURE_REPORT_RESOURCE)).getPath();
     String pathToElement = "";
+
+    List<ZipEntry> entries = listEntries(initZipFile(pathToFile), pathToElement);
+
+    assertNotNull(entries);
+    assertEquals(73, entries.size());
+  }
+
+  private ZipFile initZipFile(String pathToFile) {
     ZipFile archive;
     try {
       archive = new ZipFile(pathToFile);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-    List<ZipEntry> entries = listEntries(archive, pathToElement);
-    assertNotNull(entries);
-    assertEquals(73, entries.size());
+    return archive;
   }
 }
